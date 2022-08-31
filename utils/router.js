cb9f3234eb8b87a5671e608f173762fc43c53d88
高宇 <673395239@qq.com>
2022-08-31 15:57:28 +0800

    feat:封装所有路由

---
+++ utils/router.js
@@ -0,0 +1,19 @@
+export const Home = {
+  path: '/pages/home/index',
+  name: '首页',
+};
+
+export const Goods = {
+  path: '/pages/goods/index',
+  name: '商品',
+};
+
+export const Message = {
+  path: '/pages/message/index',
+  name: '消息',
+};
+
+export const User = {
+  path: '/pages/user/index',
+  name: '个人',
+};
